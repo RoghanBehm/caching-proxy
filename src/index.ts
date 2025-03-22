618715672f90420fe1685db5aeb8388e898372f2
Roghan <roghanthomasbehm@gmail.com>
2025-03-22 11:51:32 +1000

    Implement caching

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,78 @@
 import express from "express";
+import crypto from "crypto";
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 const app = express();
 const PORT = 3000;
+
+const CACHE_DIR = path.join(__dirname, 'cache');
+
+
+function hashRes(url: string) {
+    const key = 'GET ' + url;
+    return crypto.createHash('sha256').update(key).digest('hex');
+}
+
+async function cacheJson(requestUrl: string, jsonData: any): Promise<void> {
+    const hash = hashRes(requestUrl);
+    const filePath = path.join(CACHE_DIR, `${hash}.json`);
+  
+    await fs.mkdir(CACHE_DIR, { recursive: true });
+    await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), 'utf-8');
+}
+
+async function peekCache(requestUrl: string): Promise<boolean> {
+    const hash = hashRes(requestUrl);
+    const filePath = path.join(CACHE_DIR, `${hash}.json`);
+
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+async function getCached(requestUrl: string): Promise<any | null> {
+    const hash = hashRes(requestUrl);
+    const filePath = path.join(CACHE_DIR, `${hash}.json`);
+  
+    try {
+      const data = await fs.readFile(filePath, 'utf-8');
+      return JSON.parse(data);
+    } catch {
+      return null;
+    }
+  }
+  
 
 app.use(express.json());
 
 app.all("*", async (req, res): Promise<void> => {
 
+
+ 
+    let cache: Boolean = false;
     const path = req.path;
+
+
+    if (req.method == 'GET') {
+        
+        if (await peekCache(path)) {
+
+            const data = await getCached(path)
+
+            res.json({
+                message: "im back",
+                matchedPath: path,
+                apiResponse: data
+            });
+        }
+        cache = true;
+
+    }
+
     const isValidJson = req.headers["content-type"] === "application/json" && (req.body && Object.keys(req.body).length > 0);
 
     if (req.path === "/favicon.ico") {
@@ -27,6 +92,19 @@ app.all("*", async (req, res): Promise<void> => {
                 body: JSON.stringify(req.body),
             };
             const response = await fetch(`https://jsonplaceholder.typicode.com${path}`, fetchOptions);
+
+            const data = await response.json();
+
+            if (cache && req.method == 'GET') {
+                cacheJson(path, data);
+            }
+
+            res.json({
+                message: "im back",
+                matchedPath: path,
+                apiResponse: data
+            });
+            return;
         }
 
         const response = await fetch(`https://jsonplaceholder.typicode.com${path}`);
@@ -37,8 +115,8 @@ app.all("*", async (req, res): Promise<void> => {
 
         const data = await response.json();
 
-        const cache = {
-
+        if (cache && req.method == 'GET') {
+            cacheJson(path, data);
         }
 
         res.json({
